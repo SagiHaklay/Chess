619bbbdbf6790b4efd87f0c19865e8b23c0cc08d
Sagi Haklay <sahaklay@gmail.com>
2025-04-08 11:35:14 +0300

    Added move piece.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -5,44 +5,61 @@ class Program
     static void Main(string[] args)
     {
         ChessBoard chessBoard = new ChessBoard();
+        Player white, black;
+        white = new Player(true);
+        black = new Player(false);
+        InitializeBoard(chessBoard, black, white);
+        Console.WriteLine(chessBoard);
+        /*chessBoard.MovePiece(1, 0, 3, 0);
+        Console.WriteLine(chessBoard);
+        chessBoard.MovePiece(7, 1, 5, 2);
+        Console.WriteLine(chessBoard);*/
+    }
+
+    static void InitializeBoard(ChessBoard chessBoard, Player black, Player white)
+    {
         for (int col = 0; col < 8; col++)
         {
-            chessBoard.PlacePiece(new Pawn(true), 1, col);
-            chessBoard.PlacePiece(new Pawn(false), 6, col);
+            AddPieceToGame(new Pawn(white), 1, col, chessBoard);
+            AddPieceToGame(new Pawn(black), 6, col, chessBoard);
         }
-        chessBoard.PlacePiece(new Rook(true), 0, 0);
-        chessBoard.PlacePiece(new Rook(false), 7, 0);
-        chessBoard.PlacePiece(new Rook(true), 0, 7);
-        chessBoard.PlacePiece(new Rook(false), 7, 7);
-        chessBoard.PlacePiece(new Knight(true), 0, 1);
-        chessBoard.PlacePiece(new Knight(false), 7, 1);
-        chessBoard.PlacePiece(new Knight(true), 0, 6);
-        chessBoard.PlacePiece(new Knight(false), 7, 6);
-        chessBoard.PlacePiece(new Bishop(true), 0, 2);
-        chessBoard.PlacePiece(new Bishop(false), 7, 2);
-        chessBoard.PlacePiece(new Bishop(true), 0, 5);
-        chessBoard.PlacePiece(new Bishop(false), 7, 5);
-        chessBoard.PlacePiece(new Queen(true), 0, 3);
-        chessBoard.PlacePiece(new Queen(false), 7, 3);
-        chessBoard.PlacePiece(new King(true), 0, 4);
-        chessBoard.PlacePiece(new King(false), 7, 4);
-        Console.WriteLine(chessBoard);
+        AddPieceToGame(new Rook(white), 0, 0, chessBoard);
+        AddPieceToGame(new Rook(black), 7, 0, chessBoard);
+        AddPieceToGame(new Rook(white), 0, 7, chessBoard);
+        AddPieceToGame(new Rook(black), 7, 7, chessBoard);
+        AddPieceToGame(new Knight(white), 0, 1, chessBoard);
+        AddPieceToGame(new Knight(black), 7, 1, chessBoard);
+        AddPieceToGame(new Knight(white), 0, 6, chessBoard);
+        AddPieceToGame(new Knight(black), 7, 6, chessBoard);
+        AddPieceToGame(new Bishop(white), 0, 2, chessBoard);
+        AddPieceToGame(new Bishop(black), 7, 2, chessBoard);
+        AddPieceToGame(new Bishop(white), 0, 5, chessBoard);
+        AddPieceToGame(new Bishop(black), 7, 5, chessBoard);
+        AddPieceToGame(new Queen(white), 0, 3, chessBoard);
+        AddPieceToGame(new Queen(black), 7, 3, chessBoard);
+        AddPieceToGame(new King(white), 0, 4, chessBoard);
+        AddPieceToGame(new King(black), 7, 4, chessBoard);
+    }
+    static void AddPieceToGame(ChessPiece piece, int row, int column, ChessBoard chessBoard)
+    {
+        chessBoard.PlacePiece(piece, row, column);
+        piece.GetPlayer().AddPiece(piece);
     }
 }
 
 class ChessPiece
 {
-    bool white;
     int currentRow;
     int currentColumn;
-    public ChessPiece(bool white, int currentRow, int currentColumn) : this(white)
+    Player player;
+    public ChessPiece(Player player, int currentRow, int currentColumn) : this(player)
     {
         SetCurrentRow(currentRow);
         SetCurrentColumn(currentColumn);
     }
-    public ChessPiece(bool white)
+    public ChessPiece(Player player)
     {
-        this.white = white;
+        this.player = player;
     }
     public bool SetCurrentRow(int currentRow)
     {
@@ -66,19 +83,21 @@ class ChessPiece
     {
         return currentColumn;
     }
+    public Player GetPlayer()
+    {
+        return player;
+    }
     public override string ToString()
     {
-        return white? "W" : "B";
+        return player.IsWhite()? "W" : "B";
     }
 }
 
 class Pawn : ChessPiece
 {
 
-    public Pawn(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public Pawn(bool white) : base(white) {}
+    
+    public Pawn(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -88,10 +107,8 @@ class Pawn : ChessPiece
 class Rook : ChessPiece
 {
 
-    public Rook(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public Rook(bool white) : base(white) {}
+    
+    public Rook(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -101,10 +118,8 @@ class Rook : ChessPiece
 class Bishop : ChessPiece
 {
 
-    public Bishop(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public Bishop(bool white) : base(white) {}
+    
+    public Bishop(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -114,10 +129,8 @@ class Bishop : ChessPiece
 class Knight : ChessPiece
 {
 
-    public Knight(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public Knight(bool white) : base(white) {}
+    
+    public Knight(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -127,10 +140,8 @@ class Knight : ChessPiece
 class Queen : ChessPiece
 {
 
-    public Queen(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public Queen(bool white) : base(white) {}
+    
+    public Queen(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -140,10 +151,8 @@ class Queen : ChessPiece
 class King : ChessPiece
 {
 
-    public King(bool white, int currentRow, int currentColumn) 
-    : base(white, currentRow, currentColumn)
-    {}
-    public King(bool white) : base(white) {}
+    
+    public King(Player player) : base(player) {}
 
     public override string ToString()
     {
@@ -173,9 +182,26 @@ class ChessBoard
         board[row, column] = piece;
         return true;
     }
+    public ChessPiece? GetPiece(int row, int column)
+    {
+        if (row < 0 || row >= board.GetLength(0) || column < 0 || column >= board.GetLength(1))
+            return null;
+        return board[row, column];
+    }
+    public bool MovePiece(int startRow, int startColumn, int endRow, int endColumn)
+    {
+        ChessPiece? piece = GetPiece(startRow, startColumn);
+        if (piece == null)
+            return false;
+        bool canPlace = PlacePiece(piece, endRow, endColumn);
+        if (!canPlace)
+            return false;
+        board[startRow, startColumn] = null;
+        return true;
+    }
     public override string ToString()
     {
-        //string toColumnChar = "ABCDEFGH";
+        
         string result = "   A  B  C  D  E  F  G  H\n";
         for (int row = board.GetLength(0) - 1; row >= 0; row--)
         {
@@ -188,5 +214,33 @@ class ChessBoard
             result += '\n';
         }
         return result;
+    }
+}
+class Player
+{
+    bool white;
+    ChessPiece[] pieces;
+    int pieceCount;
+    public Player(bool white)
+    {
+        this.white = white;
+        pieces = new ChessPiece[16];
+        pieceCount = 0;
+    }
+    public bool IsWhite()
+    {
+        return white;
+    }
+    public override string ToString()
+    {
+        return string.Format("Player {0}", white? "white" : "black");
+    }
+    public void AddPiece(ChessPiece piece)
+    {
+        if (pieceCount < 16)
+        {
+            pieces[pieceCount] = piece;
+            pieceCount++;
+        }
     }
 }
